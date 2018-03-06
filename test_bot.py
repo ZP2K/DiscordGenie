88508a9f513d4948519d2d1ab5b76548495a882c
Your Name <you@example.com>
2018-03-06 00:54:37 -0500

    test file

---
+++ test_bot.py
@@ -0,0 +1,32 @@
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='.')
+extensions = ["commands"]
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print('-------------')
+
+
+@client.command()
+async def load(extension_name: str):
+    try:
+        client.load_extension(extension_name)
+    except (AttributeError, ImportError) as e:
+        await client.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+        return
+    await client.say("{} loaded.".format(extension_name))
+
+
+if __name__ == "__main__":
+    for extension in extensions:
+        try:
+            client.load_extension(extension)
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print('Failed to load extension {}\n{}'.format(extension, exc))
+
+    client.run('NDIwMzcyNTU5MzkwMDQ4MjU2.DX-96g.kU-RtfEHRa-Mt8IcxXUifIkGDss')
