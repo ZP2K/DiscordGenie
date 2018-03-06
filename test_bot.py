d8220ce9c1f37be5b11241f0636728cc8ed57a3d
Your Name <you@example.com>
2018-03-06 01:14:07 -0500

    caps

--- test_bot.py
+++ test_bot.py
@@ -4,4 +4,4 @@ from discord.ext import commands
 def test_api_status():
     client = commands.Bot(command_prefix='.')
     client.run('NDIwMzcyNTU5MzkwMDQ4MjU2.DX-96g.kU-RtfEHRa-Mt8IcxXUifIkGDss')
-    assert ("blah" in client.user.name)
+    assert ("Blah" in client.user.name)
