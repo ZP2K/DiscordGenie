64bf83de8bd94c5a9ccfc5336b7881c95423aee4
Your Name <you@example.com>
2018-03-11 00:40:07 -0500

    setting up tests

--- modules/awslambda/api.py
+++ modules/awslambda/api.py
@@ -33,6 +33,6 @@ def process(service, request):
         LogType='None',
         Payload=json.dumps(test)
     )
-    print(json.dump(response))
+    print(json.dumps(response))
 
 process("dotabuff", "mid")
