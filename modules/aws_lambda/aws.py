57f8f2d4b458bd555aa3060657808e80336f502a
Your Name <you@example.com>
2018-03-11 03:12:14 -0400

    fixes typo

--- modules/aws_lambda/aws.py
+++ modules/aws_lambda/aws.py
@@ -17,5 +17,5 @@ def process(service, request):
         LogType='None',
         Payload=json.dumps(test)
     )
-    data = response['Payload'].read().decode()
+    data = response['Payload'].read().decode('unicode_escape')
     return data
