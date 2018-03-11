f801f942274b1e59fa511db12b6b1739f82f94cb
Your Name <you@example.com>
2018-03-11 03:38:30 -0400

    int cast

--- modules/dotabuff/dotabuff.py
+++ modules/dotabuff/dotabuff.py
@@ -34,7 +34,7 @@ class Commands:
         output = "```\n"
         for i in range(10):
             stat = sort[i]
-            output += "{}, {.2g}%\n".format(stat[0], stat[1])
+            output += "{}, {.2g}%\n".format(stat[0], int(stat[1]))
         output += "```"
         return output
 
