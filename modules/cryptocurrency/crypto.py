37e4239ccbd0eb0ff72aa911555639678cad0fee
complexitydev <you@example.com>
2018-03-17 16:21:02 -0400

    proper error response check

--- modules/cryptocurrency/crypto.py
+++ modules/cryptocurrency/crypto.py
@@ -9,6 +9,19 @@ from discord.ext import commands
 from modules.aws_lambda import aws
 
 
+def get_info(request):
+    info = json.loads(aws.process("crypto", request))
+    if 'error' in info['message']:
+        return "error"
+
+    message = '```\n{}:\nUSD: {}\nBTC: {}\n24 Hour Change: {}%\n```'.format(
+        info['message'][0]['name'],
+        info['message'][0]['price_usd'],
+        info['message'][0]['price_btc'],
+        info['message'][0]['percent_change_24h'])
+    return message
+
+
 class Commands:
     def __init__(self, client):
         self.client = client
@@ -17,16 +30,9 @@ class Commands:
     async def check(self, ctx, request):
         if ctx.message.channel.id != "422504096990494741":
             return
-        info = json.loads(aws.process("crypto", request))
-        if 'error' in info['message']:
-            await self.client.say("Error looking up coin. Do not use symbol.")
-            return
-
-        message = '```\n{}:\nUSD: {}\nBTC: {}\n24 Hour Change: {}%\n```'.format(
-            info['message'][0]['name'],
-            info['message'][0]['price_usd'],
-            info['message'][0]['price_btc'],
-            info['message'][0]['percent_change_24h'])
+        message = get_info(request)
+        if message == "error":
+            await self.client.say("Error processing. Don't use the symbol name!")
 
         await self.client.say(message)
 
