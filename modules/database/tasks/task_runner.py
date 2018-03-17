1a6c49e9145124bd07bfe09ce6d46c3ae7eb59be
complexitydev <you@example.com>
2018-03-17 16:23:39 -0400

    added task reading for crypto

--- modules/database/tasks/task_runner.py
+++ modules/database/tasks/task_runner.py
@@ -2,6 +2,7 @@ import asyncio
 
 import discord
 
+from modules.cryptocurrency.crypto import get_info
 from .fetch_tasks import get_tasks
 
 
@@ -10,5 +11,7 @@ async def run_tasks(client):
     channel = discord.Object(id='422504096990494741')
     while not client.is_closed:
         tasks = get_tasks()
-        print(tasks)
+        for service, request in tasks:
+            message = get_info(request)
+            await client.send_message(channel, message)
         await asyncio.sleep(60)
