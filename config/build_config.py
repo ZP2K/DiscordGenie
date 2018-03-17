49c77c91e7c9c5d03714dac33238c9824b28c44f
complexitydev <you@example.com>
2018-03-17 16:00:16 -0400

    proper error response check

--- config/build_config.py
+++ config/build_config.py
@@ -9,3 +9,21 @@ def read_api_key():
     config = configparser.ConfigParser()
     config.read('bot.ini')
     return config['discordapi']['token']
+
+
+def read_dbname():
+    config = configparser.ConfigParser()
+    config.read('bot.ini')
+    return config['postgres']['dbname']
+
+
+def read_dbuser():
+    config = configparser.ConfigParser()
+    config.read('bot.ini')
+    return config['postgres']['dbuser']
+
+
+def read_dbpass():
+    config = configparser.ConfigParser()
+    config.read('bot.ini')
+    return config['postgres']['dbpass']
