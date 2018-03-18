3aff633ddb250d9827a7c58231eed3dbf88fce68
complexitydev <bennyh6813@gmail.com>
2018-03-18 16:26:33 -0400

    added symbol to name conversion for cmc api

--- modules/cryptocurrency/crypto.py
+++ modules/cryptocurrency/crypto.py
@@ -32,7 +32,7 @@ def get_info(request):
 
 def get_coin_list():
     info = json.loads(aws.process("lookup", "coin"))
-    return ""
+    print(info)
 
 class Commands:
     def __init__(self, client):
