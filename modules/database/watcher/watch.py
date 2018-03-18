cbf3ed465d666436d1af75505417f3f9c7e7771d
complexitydev <bennyh6813@gmail.com>
2018-03-18 16:50:32 -0400

    added symbol to name check to watch, check and convert

--- modules/database/watcher/watch.py
+++ modules/database/watcher/watch.py
@@ -5,12 +5,14 @@ import re
 
 from discord.ext import commands
 
+from modules.cryptocurrency.crypto import get_coin_list
 from modules.database.tasks.db import set_tasks
 
 
 class Commands:
     def __init__(self, client):
         self.client = client
+        self.coin_list = get_coin_list()
 
     @commands.command(pass_context=True)
     async def watch(self, ctx, service="", request=""):
@@ -23,6 +25,9 @@ class Commands:
         if not r:
             await self.client.say("Invalid request")
             return
+        r = re.search("[A-Z]+", request)
+        if r:
+            request = self.coin_list['Data'][request]['CoinName']
         set_tasks(request)
         await self.client.say("```\nWatch request for Service: {} Request: {} added\n```".format(service, request))
 
